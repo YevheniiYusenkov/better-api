de9e301afbd61d38e1e20829441c7de296486d45
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-13 12:38:30 +0200

    imported TypeOrmModule with entities User, Account

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { GatewayConfigModule } from '@better-api/config';
+import { User, Account } from '@better-api/entities';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +10,19 @@ import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'better',
+      synchronize: true,
+      entities: [
+        User,
+        Account,
+      ],
+    }),
     GatewayConfigModule,
     UsersModule,
   ],
