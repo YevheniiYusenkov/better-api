2c2f7b5f20ca5d4e390bf940374bae1c34a34346
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-13 02:16:09 +0200

    created GatewayConfigModule

--- libs/config/src/gateway/config.module.ts
+++ libs/config/src/gateway/config.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import config from './config';
+import { GatewayConfigService } from './config.service';
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [ config ],
+    }),
+  ],
+  providers: [ ConfigService, GatewayConfigService ],
+  exports: [ GatewayConfigService ],
 })
-export class ConfigModule {}
+export class GatewayConfigModule {}
--- libs/config/src/index.ts
+++ libs/config/src/index.ts
@@ -1,1 +1,2 @@
-export * from './lib/config.module';
+export * from './gateway/config.module';
+export * from './gateway/config.service';
