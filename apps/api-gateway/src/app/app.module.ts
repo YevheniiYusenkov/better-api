82f7ae7317b3ebf660259584892c5ee799f83722
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-13 03:17:29 +0200

    imported UsersModule

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 
-import { GatewayConfigModule } from "@better-api/config";
+import { GatewayConfigModule } from '@better-api/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [ GatewayConfigModule ],
+  imports: [
+    GatewayConfigModule,
+    UsersModule,
+  ],
   controllers: [ AppController ],
   providers: [ AppService ],
 })
