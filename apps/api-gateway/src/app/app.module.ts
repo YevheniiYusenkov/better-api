222a656b79dd88cb41b561b3ff1579faa204c8d1
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-13 02:16:47 +0200

    imported GatewayConfigModule

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { GatewayConfigModule } from "@better-api/config";
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ GatewayConfigModule ],
+  controllers: [ AppController ],
+  providers: [ AppService ],
 })
 export class AppModule {}
